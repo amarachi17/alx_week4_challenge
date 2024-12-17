17aed5b8a6d38a15f44116044a909a13a6c12dc4
Confidence <nkeeonyeconfidence@gmail.com>
2024-12-17 13:48:07 +0100

    sum_of_numbers.py

---
+++ sum_of_numbers.py
@@ -0,0 +1,9 @@
+# Creating a list of numbers
+numbers = [1, 5, 3, 9]
+total = 0
+
+# creating a for loop
+for number in numbers:
+    total += number
+    
+print(f"The sum of all the number is {total}")
